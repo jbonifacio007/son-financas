d89d159e07da8eb97d2f403c448d83b59db5e86b
bonifacio <jbonifacio07@gmail.com>
2017-06-10 20:46:17 -0300

    Refatorando model

--- src/Plugins/DbPlugin.php
+++ src/Plugins/DbPlugin.php
@@ -2,6 +2,11 @@
 declare(strict_types=1);
 namespace SONFin\Plugins;
 
+
+use Interop\Container\ContainerInterface;
+
+use SONFin\Repository\RepositoryFactory;
+
 use SONFin\ServiceContainerInterface;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
@@ -13,5 +18,11 @@ class DbPlugin implements PluginInterface
         $config = include __DIR__ . '/../../config/db.php';
         $capsule->addConnection($config['development']);
         $capsule->bootEloquent();
+
+        $container->add('repository.factory', new RepositoryFactory());
+		$container->addLazy('category-cost.repository', function (ContainerInterface $container) {
+		    return $container->get('repository.factory')->factory(CategoryCost::class);
+		});
+
     }
 }
---
+++ src/controllers/category-costs.php
@@ -0,0 +1,90 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use SONFin\Models\CategoryCost;
+
+$app
+
+    ->get('/category-costs2', function() use($app){
+        $view = $app->service('view.renderer');
+        return $view->render('category-costs/list.html.twig');
+
+    })
+
+  ->get('/category-costs3', function() use($app){
+        $view = $app->service('view.renderer');
+        $meuModel = new \SONFin\Models\CategoryCost();
+        $categories = $meuModel->all();
+
+        return $view->render('category-costs/list.html.twig',[
+            'categories' => $categories
+        ]);
+
+    },'category-costs.list3')
+
+
+
+  ->get('/category-costs', function() use($app){
+        $view = $app->service('view.renderer');
+
+        $repository = $app->service('category-cost.repository');
+        $categories = $repository->all();
+        
+        return $view->render('category-costs/list.html.twig',[
+            'categories' => $categories
+        ]);
+
+    },'category-costs.list')
+
+
+  ->get('/category-costs/new', function() use($app){
+        $view = $app->service('view.renderer');
+        return $view->render('category-costs/create.html.twig');
+
+    },'category-costs.new')
+
+  ->post('/category-costs/store', function (ServerRequestInterface $request) use ($app){
+        $data = $request->getParsedBody();
+        \SONFin\Models\CategoryCost::create($data);
+        return $app->route('category-costs.list');
+
+    },'category-costs.store')
+
+  ->get('/category-costs/{id}/edit', function( ServerRequestInterface $request) use($app){
+        $view = $app->service('view.renderer');
+        $id = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+
+        return $view->render('category-costs/edit.html.twig',[
+            'category' => $category
+        ]);
+
+    },'category-costs.edit')
+
+  ->post('/category-costs/{id}/update', function( ServerRequestInterface $request) use($app){
+        $id = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+        $data = $request->getParsedBody();
+        $category->fill($data);
+        $category->save();
+        return $app->route('category-costs.list');
+
+    },'category-costs.update')
+    
+    ->get('/category-costs/{id}/show', function(ServerRequestInterface $request) use($app){
+        $view = $app->service('view.renderer');
+        $id = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+        return $view->render('category-costs/show.html.twig', [
+            'category' => $category
+        ]);
+    }, 'category-costs.show')
+
+    ->get('/category-costs/{id}/delete', function(ServerRequestInterface $request) use($app){
+        $id = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+        $category->delete();
+        return $app->route('category-costs.list');
+    }, 'category-costs.delete')
+    
+  ;
